d46456449c04d0206b9e86e86b07d6ab289faee9
Monther Yasin <monther.yasin@gmail.com>
2017-10-09 12:28:53 -0400

    adding few more hosts

--- hosts.pp
+++ hosts.pp
@@ -1,15 +1,23 @@
 host { 'superman':
-	ip => '192.168.126.131',
+  ip => '192.168.126.131',
 }
 
-host {'spiderman': 
-	ip => '192.168.126.132',
+host {'spiderman':
+  ip => '192.168.126.132',
 }
 
-host {'batman': 
-	ip => '192.168.126.133',
+host {'batman':
+  ip => '192.168.126.133',
 }
 
-host {'hisman': 
-	ip => '192.168.126.134',
- 
+host {'lazyman':
+  ip => '192.168.126.134',
+}
+
+host { 'willyman':
+  ip => '192.168.126.135',
+}
+
+host { 'evensteven':
+  ip => '192.168.126.136',
+}
